d7c6d60bcefbcde31e73498bbe799257e6f839d9
djon198360 <le1907@mail.ru>
2023-07-08 19:13:07 +0300

    feat: show result

--- js_component/renderScreenOver.js
+++ js_component/renderScreenOver.js
@@ -1,42 +1,47 @@
 import { app } from "../index.js";
 import { unitCheck } from "./functions.js";
 import { timer } from "./renderScreenGame.js";
-function gameOver(text) {
+function gameOver(text, imgs, minutes, seconds) {
       let fragment = new DocumentFragment();
       let screen = document.createElement("div");
       screen.classList.add('screen');
       let p = document.createElement("p");
-      p.classList.add('title');
+      p.classList.add('title_' + imgs);
       p.textContent = text;
+      let img = document.createElement("img");
+      img.classList.add(imgs);
+      img.setAttribute('src', './img/' + imgs + '.svg');
+      let titleTime = document.createElement("p");
+      titleTime.textContent = "Затраченное время:";
+      titleTime.classList.add('title_time');
+      let time = document.createElement('span');
+      time.textContent = `${minutes.toString().padStart(2, '0')}.${seconds.toString().padStart(2, '0')}`;
+      time.classList.add('time_over');
       let button = document.createElement("button");
-      button.textContent = "Начать заново";
-button.classList.add('start');
+      button.textContent = "Играть снова";
+      button.classList.add('start');
+
+
+      screen.appendChild(img);
       screen.appendChild(p);
+      screen.appendChild(titleTime);
+      screen.appendChild(time);
       screen.appendChild(button);
       fragment.append(screen);
 
       return fragment;
 }
-/*                      <div class="screen">  
-                            <p class="title">${textStart}</p>
-                            <div class="qual">
-                            <div class="but one">1</div>
-                            <div class="but two">2</div>
-                            <div class="but free">3</div>
-                            </div>
-                            <button class="start" disabled="true">Старт</button>
-                      </div>
-*/ 
 
-export function renderOver(text) {
-    
-
-app.append(gameOver(text));
-clearTimeout(timer);
-document.querySelector('.start').addEventListener('click', (e) => {
-      localStorage.clear();
-      unitCheck('unit', 'Start');
-});
+export function renderOver(text, imgs, minutes, seconds) {
+      let div = document.createElement("div");
+      div.classList.add('layout');
+app.append(div);
+      app.append(gameOver(text, imgs, minutes, seconds));
+      clearTimeout(timer);
+      document.querySelector('.start').addEventListener('click', (e) => {
+            localStorage.clear();
+            unitCheck('unit', 'Start');
+      });
 
 
 }
