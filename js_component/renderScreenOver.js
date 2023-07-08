522329b30cd414f9a856ef9b7d2569df93a469a3
djon198360 <le1907@mail.ru>
2023-07-08 16:38:17 +0300

    fix

--- js_component/renderScreenOver.js
+++ js_component/renderScreenOver.js
@@ -1,11 +1,42 @@
 import { app } from "../index.js";
+import { unitCheck } from "./functions.js";
+import { timer } from "./renderScreenGame.js";
+function gameOver(text) {
+      let fragment = new DocumentFragment();
+      let screen = document.createElement("div");
+      screen.classList.add('screen');
+      let p = document.createElement("p");
+      p.classList.add('title');
+      p.textContent = text;
+      let button = document.createElement("button");
+      button.textContent = "Начать заново";
+button.classList.add('start');
+      screen.appendChild(p);
+      screen.appendChild(button);
+      fragment.append(screen);
 
-export function renderOver() {
-    const textStart = 'Результат';
-    app.innerHTML = `
-                      <div class="screen">  
+      return fragment;
+}
+/*                      <div class="screen">  
                             <p class="title">${textStart}</p>
+                            <div class="qual">
+                            <div class="but one">1</div>
+                            <div class="but two">2</div>
+                            <div class="but free">3</div>
+                            </div>
+                            <button class="start" disabled="true">Старт</button>
                       </div>
-                `;
+*/ 
+
+export function renderOver(text) {
+    
+
+app.append(gameOver(text));
+clearTimeout(timer);
+document.querySelector('.start').addEventListener('click', (e) => {
+      localStorage.clear();
+      unitCheck('unit', 'Start');
+});
+
 
 }
