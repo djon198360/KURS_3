1dffeb872f38bb505d048643b28c2f496cf9f260
djon198360 <le1907@mail.ru>
2023-07-11 19:55:02 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
- const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); 
-const MiniCssExtractPlugin = require ("mini-css-extract-plugin");
-const CssMinimizerWebpackPlugin = require ("css-minimizer-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
 	mode: 'production',
@@ -10,29 +10,37 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'main.js',
 		clean: true,
-	  },
-	module:{
-		rules: [
-			{ test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader' ] },
-			//{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-			{
-			  test: /\.(png|svg|jpg|jpeg|gif)$/i,
-			  type: "asset/resource",
-			},
-			{
-			  test: /\.(woff|woff2|eot|ttf|otf)$/i,
-			  type: "asset/resource",
-			},
-		  ]
 	},
-	
-	optimization:{
-		minimizer:["...", new CssMinimizerWebpackPlugin()],
-	},
-	plugins:[new HtmlWebpackPlugin({
-			template:"index.html",
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'index.html',
 		}),
-		new MiniCssExtractPlugin(),
-	],   
-	/*убрать для продакшн*/ 	devtool: "source-map",
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// all options are optional
+			filename: 'style.css',
+/* 			chunkFilename: '[id].css',
+			ignoreOrder: false, // Enable to remove warnings about conflicting order */
+		  }),
+	],
+	module: {
+		rules: [
+			
+		{test: /\.css$/i,use: [MiniCssExtractPlugin.loader, 'css-loader'],}, 
+		//	{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	optimization: {
+		minimizer: ['...', new CssMinimizerWebpackPlugin()],
+	},
+	/*убрать для продакшн*/ devtool: 'source-map',
 }
