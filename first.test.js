a6b54d7f6e8c5c33bd612155c52055f636d78b1a
djon198360 <le1907@mail.ru>
2023-07-24 10:48:37 +0300

    add test

--- first.test.js
+++ first.test.js
@@ -2,27 +2,11 @@
  * @jest-environment jsdom
  */
 
-//import { getListContent } from "./js_component/renderScreenGame";
 
-//const {PAGE} = require("./index");
-//const {getListContent} = require("./js_component/renderScreenGame")
 const {it, expect, describe} = require("@jest/globals");
-//import { it, expect }  from ("@jest/globals");
-/*   it("Проверяем получения значиний из локалсторедж", () => {
-   const minus = require('./sum');
-    expect(minus(5,3)).toBe(9);
-  });    */
 
 describe("test game", () => {
-	/* 	describe("test index page", () => {
-		it("бесполезная проверка", () => {
-			const unit = require("./index");
-
-			expect(PAGE).toEqual(unit.PAGE);
-		});
-	}); */
 	let mockFridge = {};
-
 	beforeEach(() => {
 		global.Storage.prototype.setItem = jest.fn((key, value) => {
 			mockFridge[key] = value;
@@ -34,12 +18,9 @@ describe("test game", () => {
 
 	it("game test get", () => {
 		global.Storage.prototype.setItem("qual", 1);
-
 		const {getListContent} = require("./js_component/renderScreenGame");
-
-		const cards1 = getListContent(); //Пробоал getListContent
+		const cards1 = getListContent(); 
 		const cards2 = getListContent();
-
-		expect(cards1).not.toEqual(cards2); //пробовал разные вариации cards1.Array cards1.newArr
+		expect(cards1).not.toEqual(cards2);
 	});
 });
